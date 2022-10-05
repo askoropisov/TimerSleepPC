ec98343503b4cfeaccb54d447374025fc39648b9
Артем Cкорописов <sckoropisow.ar@yandex.ru>
2022-10-05 11:24:31 +0300

    Добавил режим выключения и прозрачный фон

--- TimerSleepPC/ViewModels/HomeWindowViewModel.cs
+++ TimerSleepPC/ViewModels/HomeWindowViewModel.cs
@@ -1,62 +1,44 @@
-﻿using Avalonia;
-using ReactiveUI;
+﻿using ReactiveUI;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
-using TimerSleepPC.ViewModels;
 
 namespace TimerSleepPC.ViewModels
 {
     public class HomeWindowViewModel : ViewModelBase
     {
-        private Stopwatch _stopwatch;
-        Timer timer;
+        private Stopwatch? _stopwatch;
+        private Timer? timer;
 
-        public HomeWindowViewModel()
-        {
-            timer = new Timer(CallBack, null, Timeout.Infinite, 5 * 1000);
-        }
-
-        private string _minute;
-        public string Minuts
+        private string? _minute;
+        public string? Minuts
         {
             get => _minute;
-            set
-            {
-                this.RaiseAndSetIfChanged(ref _minute, value);
-            }
+            set => this.RaiseAndSetIfChanged(ref _minute, value);
         }
 
-        private string _hours;
-        public string Hours
+        private string? _hours;
+        public string? Hours
         {
             get => _hours;
-            set
-            {
-                this.RaiseAndSetIfChanged(ref _hours, value);
-            }
+            set => this.RaiseAndSetIfChanged(ref _hours, value);
         }
 
-        private string _time;
-        public string Time
-        {
-            get => _time;
-            set
-            {
-                this.RaiseAndSetIfChanged(ref _time, value);
-            }
-        }
+        public int AllTime { get; set; }
 
         private bool _PCOff;
         public bool PCOff
         {
             get => _PCOff;
             set => this.RaiseAndSetIfChanged(ref _PCOff, value);
+        }
+
+        private bool _mode = true;
+        public bool Mode
+        {
+            get => _mode;
+            set => this.RaiseAndSetIfChanged(ref _mode, value);
         }
 
         private TimeSpan? _remainTime;
@@ -66,34 +48,34 @@ namespace TimerSleepPC.ViewModels
             set => this.RaiseAndSetIfChanged(ref _remainTime, value);
         }
 
-        public void SetTime()
+        public void SleepOrPowerOff()
         {
             [DllImport("Powrprof.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
             static extern bool SetSuspendState(bool hiberate, bool forceCritical, bool disableWakeEvent);
 
-            SetSuspendState(false, true, true);
+            CancelTimer();
+            if(Mode) SetSuspendState(false, true, true);
+            else Process.Start("shutdown", "/s /t 0");
         }
 
         public void GoTimer()
         {
+            timer = new Timer(CallBack, null, Timeout.Infinite, 5 * 1000);
             PCOff = true;
+
             int time = Convert.ToInt32(Minuts) * 60 + Convert.ToInt32(Hours) * 3600;
             AllTime = time;
 
             _stopwatch = new Stopwatch();
             _stopwatch.Start();
 
-            //Timer timer2 = new Timer(CallBack, 0, 0, 1000);
             timer.Change(0, 1000);
         }
-
-        public int AllTime { get; set; }
 
         public void CallBack(object? state)
         {
             RemainTime = TimeSpan.FromSeconds(AllTime) - _stopwatch.Elapsed;
-            if (RemainTime.Value.TotalSeconds >= 0 && RemainTime.Value.TotalSeconds <= 1) SetTime();
-            //Task.Delay(200);
+            if (RemainTime.Value.TotalSeconds >= 0 && RemainTime.Value.TotalSeconds <= 1) SleepOrPowerOff();
         }
 
         public void CancelTimer()
